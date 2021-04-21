16ae73596b3adfff0b3cbf18dae2363f38fc5996
Olivia Victorino <ovictor@uw.edu>
2021-04-20 18:08:50 -0700

    with svg

--- src/App.js
+++ src/App.js
@@ -6,7 +6,13 @@ const vw = 500;
 const App = () => {
 return <svg style={{border: "1px solid pink", width:vw, height:vh}}>Hello World from React
 
-
+<circle cx={vh} cy={vw} r="5" />
+<rect fill="rgb(230,230,230)" width={10} height={10}x={200} y={100} />
+<rect fill="rgb(230,230,230)" width={10} height={10}x={212} y={100} />
+<rect fill="rgb(230,230,230)" width={10} height={10}x={224} y={100} />
+<rect fill="rgb(230,230,230)" width={10} height={10}x={236} y={100} />
+<rect width={10} height={10}x={248} y={100} fill="red"/>
+<line x1={0} y1={vh} x2={150} y2={100} stroke="black" />
 
 <p> April 13 + 7</p>
 
